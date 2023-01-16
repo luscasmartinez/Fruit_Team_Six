4d346c033ba5a1e78e90692a15d9bad66909fe9d
luscasmartinez <lucasmartinez.aluno@unipampa.edu.br>
2023-01-16 10:30:49 -0300

    10:30 16/01/2023

--- Fruit/src/cadastros/CadItem.java
+++ Fruit/src/cadastros/CadItem.java
@@ -1,5 +1,8 @@
 package cadastros;
 
+import java.util.LinkedList;
+
 public class CadItem {
+    private LinkedList<CadItem> cadItem;
     
 }
--- Fruit/src/construtores/NotaFiscal.java
+++ Fruit/src/construtores/NotaFiscal.java
@@ -1,5 +1,37 @@
 package construtores;
 
+import java.util.Date;
+
 public class NotaFiscal {
+    private int codigo;
+    private Date dataEmissao;
+    private Item item;
+
     
+
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getDataEmissao() {
+        return this.dataEmissao;
+    }
+
+    public void setDataEmissao(Date dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+
+    public Item getItem() {
+        return this.item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
 }
